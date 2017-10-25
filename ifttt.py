a7cdd019a737010e01f701da22e1909ef5dd3469
Alan Bidart <alanbidart@gmail.com>
2017-10-24 22:00:59 -0400

    Added ifttt.py

--- ifttt.py
+++ ifttt.py
@@ -1,6 +1,29 @@
 import sys
-print(sys.argv)
 
-def main():
-    if sys.argv[1] != "if":
-        print ""
+#runs if the user calls the script and does not have experience running it
+def startup():
+    print ("Welcome to If This Then That Blockchain version")
+    print ("type 'help' if you would like to learn more or press 'enter' to start")
+    while raw_input(":")=='help':
+        print "bla bla bla"
+    ifClause = raw_input("If:")
+    thenClause = raw_input("Then:")
+    print ("Your smart contracts states that if " + ifClause + " then " + thenClause)
+    if  raw_input("Is this correct? [y/n]") == 'n':
+        print ("Unlucky")
+    #editing of the solidity text shoudl happen here
+    print ("Your contract has been pushed to the chain. HYPE.")
+    return
+
+#runs if exactly two arguments are passed on from the command line
+def proStartup(ifClause, thenClause):
+    print ("Your smart contracts states that if " + ifClause + " then " + thenClause)
+    #editing of the solidity text shoudl happen here
+    print ("Your contract has been pushed to the chain. HYPE.")
+    return
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        proStartup(sys.argv[1], sys.argv[2])
+    else:
+        startup()
